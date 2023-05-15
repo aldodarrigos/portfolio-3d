2085af4880f2924788bc3f21e4ed73dcee1d0f2c
Aldo D'Arrigo <aldodarrigos@gmail.com>
2023-05-15 06:58:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact,Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works, StarsCanvas } from './components'
+
+function App() {
+
+  return (
+    <div>
+        3D Developer Portfolio
+    </div>
+  )
+}
+
+export default App

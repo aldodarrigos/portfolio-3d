bc51a1c67653cb701677a1d25a45c72166ba3f33
Aldo D'Arrigo <aldodarrigos@gmail.com>
2023-05-17 23:22:16 -0500

    work on navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { styles } from '../styles'
+import { Link, NavLink } from 'react-router-dom'
+import {  logo } from "../assets";
 const Navbar = () => {
+
+  const [active, setActive] = useState("")
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link 
+          to="/"
+          className='flex items-center gap-2'
+          onClick={ () => {
+            setActive('');
+            window.scrollTo(0,0)
+          }}
+        >
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p className="text-white text-[10px] font-bold cursor-pointer">Aldo D'Arrigo 
+            <span className='sm-block hidden'>
+             | Web Developer
+            </span>
+          </p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
